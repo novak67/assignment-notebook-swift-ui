d0ac1cf211b6d55d61abe9fb83d8ada68154ce2d
Ryan Bachrach <ryanbachrach9@gmail.com>
2021-03-13 16:57:16 -0600

    Initial Commit

---
+++ assignment notebook swift uiTests/assignment_notebook_swift_uiTests.swift
@@ -0,0 +1,33 @@
+//
+//  assignment_notebook_swift_uiTests.swift
+//  assignment notebook swift uiTests
+//
+//  Created by ryan b on 3/13/21.
+//
+
+import XCTest
+@testable import assignment_notebook_swift_ui
+
+class assignment_notebook_swift_uiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
